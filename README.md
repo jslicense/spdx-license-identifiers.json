b20d67e0b5c7893e662c3e8be2ce765a3f7d1208
Kyle E. Mitchell <kyle@kemitchell.com>
2020-08-31 11:15:08 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# spdx-license-identifiers
+
+The package exports an array of strings. Each string is the [Software Package Data Exchange (SPDX)](https://spdx.org) identifier for a license, according to the SPDX [license list](https://spdx.org/licenses/).
+
+## History
+
+This package was created from scratch as a replacement for [spdx-license-ids](https://npmjs.com/package/spdx-license-ids) in August of 2020, to incorporate many new license identifiers in recent releases of the SPDX license list.  The maintainer of spdx-license-ids, [@shinnn](https://github.com/shinnn), served the JavaScript community for years by updating that package for new versions of the SPDX license list.
+
+## Licensing
+
+### Software Package Data eXchange
+
+"SPDX" is a federally registered United States trademark of The Linux Foundation Corporation.  This package is not a product of, or in any way endorsed by, The Linux Foundation.
+
+From version 2.2 of the SPDX specification:
+
+> Copyright © 2010-2020 Linux Foundation and its Contributors. This work is licensed under the Creative Commons Attribution License 3.0 Unported (CC-BY-3.0) reproduced in its entirety in Appendix VII herein. All other rights are expressly reserved.
+
+### This Package
+
+This package is created by downloading and processing the machine-readable JSON version of SPDX' license list from spdx.org, a purely mechanical and routine exercise involving no creativity whatever.  As a result, the package maintainer's believe that none of the data files included in the distribution tarballs of this package come under copyright.
+
+Copyrights in any other content, such as the build script and this `README.md` file, is dedicated to the public domain under [Creative Commons' CC0 1.0 Universal Public Domain Dedication](https://creativecommons.org/publicdomain/zero/1.0/).
---
+++ build.js
@@ -0,0 +1,62 @@
+#!/usr/bin/env node
+const assert = require('assert')
+const fs = require('fs')
+const https = require('https')
+
+// Download JSON file from spdx.org.
+https.request('https://spdx.org/licenses/licenses.json')
+  .once('response', function (response) {
+    assert.strictEqual(response.statusCode, 200)
+    const chunks = []
+    response
+      .on('data', function (chunk) { chunks.push(chunk) })
+      .once('error', function (error) { assert.ifError(error) })
+      .once('end', function () {
+        const buffer = Buffer.concat(chunks)
+        let parsed
+        try {
+          parsed = JSON.parse(buffer)
+        } catch (error) {
+          assert.ifError(error)
+        }
+
+        // Read list version from JSON.
+        const version = parsed.licenseListVersion
+        assert.strictEqual(typeof version, 'string')
+
+        // Read identifiers from JSON.
+        const licenses = parsed.licenses
+        assert.strictEqual(Array.isArray(licenses), true)
+        const index = []
+        const deprecated = []
+        licenses.forEach(function (object) {
+          const id = object.licenseId
+          assert.strictEqual(typeof id, 'string')
+          if (object.isDeprecatedLicenseId) {
+            deprecated.push(id)
+          } else {
+            index.push(id)
+          }
+        })
+
+        // Sort identifier lists.
+        index.sort()
+        deprecated.sort()
+
+        // Write JSON files.
+        let todo = 3
+        function writeFile (path, data) {
+          fs.writeFile(
+            path, JSON.stringify(data, null, 2) + '\n',
+            function (error) {
+              assert.ifError(error)
+              if (--todo === 0) process.exit(0)
+            }
+          )
+        }
+        writeFile('version.json', version)
+        writeFile('index.json', index)
+        writeFile('deprecated.json', deprecated)
+      })
+  })
+  .end()
