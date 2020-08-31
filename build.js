157f7eee6e3fe32536bc41906bdc76d2c9174572
Kyle E. Mitchell <kyle@kemitchell.com>
2020-08-31 11:23:08 -0700

    Write SPDX license list version to package version

--- build.js
+++ build.js
@@ -45,18 +45,33 @@ https.request('https://spdx.org/licenses/licenses.json')
 
         // Write JSON files.
         let todo = 3
+        function finishTask (error) {
+          assert.ifError(error)
+          if (--todo === 0) process.exit(0)
+        }
         function writeFile (path, data) {
           fs.writeFile(
             path, JSON.stringify(data, null, 2) + '\n',
-            function (error) {
-              assert.ifError(error)
-              if (--todo === 0) process.exit(0)
-            }
+            finishTask
           )
         }
-        writeFile('version.json', version)
         writeFile('index.json', index)
         writeFile('deprecated.json', deprecated)
+        fs.readFile(
+          'package.json',
+          function (error, buffer) {
+            assert.ifError(error)
+            let parsed
+            try {
+              parsed = JSON.parse(buffer)
+            } catch (error) {
+              assert.ifError(error)
+            }
+            const patch = process.env.PATCH || '0'
+            parsed.version = version + '.' + patch
+            writeFile('package.json', parsed)
+          }
+        )
       })
   })
   .end()
